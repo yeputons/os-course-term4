883765390a6a452635f90d9cd5e65fb34b1083f5
Egor Suvorov <egor.suvorov@gmail.com>
2016-02-23 18:12:40 +0300

    Move printf core to printf_impl.c; implement snprintf

--- printf.h
+++ printf.h
@@ -2,8 +2,11 @@
 #define _PRINTF_H
 
 #include <stdarg.h>
+#include <stddef.h>
 
 int printf(const char *format, ...);
 int vprintf(const char *format, va_list args);
+int snprintf(char *outbuf, size_t outbufsize, const char* format, ...);
+int vsnprintf(char *outbuf, size_t outbufsize, const char* format, va_list args);
 
 #endif
