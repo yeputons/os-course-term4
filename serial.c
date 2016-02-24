fc599a9b01905e03a25ca8509417333cc0519abb
Egor Suvorov <egor.suvorov@gmail.com>
2016-02-24 21:55:35 +0300

    serial: add check for TRANSMIT_OVER_BIT in Line Status Register

--- serial.c
+++ serial.c
@@ -9,12 +9,15 @@
 #define SPD_HIGH_PORT (BASE_PORT + 1)
 
 #define LCR_PORT (BASE_PORT + 3)
+#define STATUS_PORT (BASE_PORT + 5)
 
 #define DLAB (1 << 7)
 
 // Serial port config: 8 bit, no stop bit, no parity check
 #define SERIAL_CONTROL 3
 #define SERIAL_DIVISOR_LATCH 0x000C // 9600 bps
+
+#define TRANSMIT_OVER_BIT (1 << 5)
 
 void init_serial(void) {
     out8(LCR_PORT, DLAB | SERIAL_CONTROL);
@@ -26,6 +29,7 @@ void init_serial(void) {
 
 void putchar(int c) {
     out8(DATA_PORT, c);
+    while (!(in8(STATUS_PORT) & TRANSMIT_OVER_BIT));
 }
 
 void puts(char *s) {
