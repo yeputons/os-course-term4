93e213e004d8eac948fe5f7418bdbd38db20d442
Egor Suvorov <egor.suvorov@gmail.com>
2016-03-15 01:04:58 +0300

    main.print_memory_map: add heading zeros in memory addresses

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ void print_memory_map(void) {
     uint32_t pos = 0;
     while (pos < mboot_info->mmap_length) {
         struct mboot_memory_segm *segm = (void*)(uint64_t)(mboot_info->mmap_addr + pos);
-        printf("Memory segment [%p..%p] is %s\n", segm->base_addr, segm->base_addr + segm->length - 1, segm->type == 1 ? "available" : "unavailable");
+        printf("Memory segment [%016p..%016p] is %s\n", segm->base_addr, segm->base_addr + segm->length - 1, segm->type == 1 ? "available" : "unavailable");
         pos += segm->size + 4;
     }
 }
