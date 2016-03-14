944ec76436c2e401f1d0db31f5cbd9e1e7996310
Egor Suvorov <egor.suvorov@gmail.com>
2016-03-15 02:51:11 +0300

    util.h: add assert()

--- util.h
+++ util.h
@@ -5,5 +5,6 @@
 
 size_t strlen(const char *s);
 void die(const char *format, ...);
+#define assert(cond) if (!(cond)) { die("Assertion failed at %s:%d: %s\n", __FILE__, __LINE__, #cond); }
 
 #endif
