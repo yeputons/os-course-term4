7da21cf3ff089ff199136ce8a41d3f729ff8f2c3
Egor Suvorov <egor.suvorov@gmail.com>
2016-03-15 03:10:16 +0300

    memory: fix memory map printing; add more info

--- memory.c
+++ memory.c
@@ -6,6 +6,9 @@
 
 struct buddy_allocator first_buddy;
 
+extern char text_phys_begin[];
+extern char bss_phys_end[];
+
 void init_memory(void) {
     if (!(mboot_info->flags & (1 << 6))) {
         die("Memory map unavailable from multiboot\n");
@@ -14,7 +17,7 @@ void init_memory(void) {
     uint32_t pos = 0;
     while (pos < mboot_info->mmap_length) {
         struct mboot_memory_segm *segm = (void*)(uint64_t)(mboot_info->mmap_addr + pos);
-        printf("Memory segment [%016p..%016p] is %s\n", segm->base_addr, segm->base_addr + segm->length - 1, segm->type == 1 ? "available" : "unavailable");
+        printf("Memory segment [%016p..%016p) is %s\n", segm->base_addr, segm->base_addr + segm->length - 1, segm->type == 1 ? "available" : "unavailable");
         pos += segm->size + 4;
     }
     
@@ -24,5 +27,7 @@ void init_memory(void) {
     if (mboot_info->mmap_addr + mboot_info->mmap_length > 1024 * 1024) {
         die("mboot_info->mmap ends in upper memory");
     }
+    printf("Kernel resides [%016p..%016p)\n", text_phys_begin, bss_phys_end);
+
     buddy_init(&first_buddy, 0);
 }
