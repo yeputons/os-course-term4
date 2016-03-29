268cdbb4230ccca1f38433b4bc3aa151a9bb35c2
Egor Suvorov <egor.suvorov@gmail.com>
2016-03-29 13:31:14 +0300

    buddy.h: get rid of 21-bit fields

--- buddy.h
+++ buddy.h
@@ -8,8 +8,8 @@
 #define BUDDIES_CNT (1 << (BUDDY_LEVELS + 1))
 
 struct buddy_info {
-    int prev_free:(BUDDY_LEVELS + 2); // 2**(BUDDY_LEVELS+1) nodes in total, plus one sign bit
-    int next_free:(BUDDY_LEVELS + 2);
+    int prev_free;
+    int next_free;
     unsigned is_free:1;
     unsigned allocated_level:5;
 };
