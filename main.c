731e2fe81fb58bfb7ec79aef84347d350350d5c7
Egor Suvorov <egor.suvorov@gmail.com>
2016-03-30 11:50:29 +0300

    main: add GPE and PF handler

--- main.c
+++ main.c
@@ -12,6 +12,19 @@ void timer(struct interrupt_info *info) {
     printf("Timer! step=%d\n", timer_step++);
 }
 
+void fault_handler(struct interrupt_info *info) {
+	(void)info;
+	__asm__("cli");
+	printf("FAIL interrupt_id=%d, error_code=%d\n", info->interrupt_id, info->error_code);
+	printf("rip=0x%p, cs=%d, rflags=0x%08llx\n", info->rip, info->cs, info->rflags);
+	#define dump(reg) printf("  %3s=0x%016llx\n", #reg, info->reg)
+	dump(rax); dump(rbx); dump(rcx); dump(rdx);
+	dump(rdi); dump(rsi); dump(rbp); dump(rsp);
+	dump( r8); dump( r9); dump(r10); dump(r11);
+	dump(r12); dump(r13); dump(r14); dump(r15);
+	die("");
+}
+
 void main(void) {
     __asm__("cli");
     init_serial();
@@ -20,6 +33,9 @@ void main(void) {
     printf("Loading IDT... ");
     init_idt();
     printf("OK\n");
+
+    set_int_handler(13, fault_handler); // General protection fault
+    set_int_handler(14, fault_handler); // Page fault
 
     printf("Starting PIC... ");
     init_pic();
